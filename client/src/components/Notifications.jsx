10ff16488e02274b8e38ae560d31b23f4265225b
jyotijoey <jburnwal5@gmail.com>
2021-05-20 17:43:49 +0530

    notifications component created

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {Button} from "@material-ui/core"
+import {SocketContext} from "../SocketContext"
 
 function Notifications() {
+
+    const {answerCall, call, callAccepted} = useContext(SocketContext);
     return (
-        <div>
-            <h1>Notifications</h1>
-        </div>
-    )
+        <>
+            {call.isRecievedCall && !callAccepted && (
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <h1>{call.name} is calling: </h1>
+                    <Button variant="contained" color="primary" onClick={answerCall}>
+                        Answer Call
+                    </Button>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default Notifications
